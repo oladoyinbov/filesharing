358e287ee2d005484b1b468000a47a3c45c4bab2
Oladoyinbo Vincent <oladoyinboadverts@gmail.com>
2023-12-20 09:27:30 +0100

    :memo: wip

--- app/Http/Controller/HomeController.php
+++ app/Http/Controller/HomeController.php
@@ -4,22 +4,12 @@ declare(strict_types=1);
 
 namespace App\Http\Controller;
 
-use Fastvolt\Core\Http\{HttpRequest as Request, HttpResponse as Response};
+use Fastvolt\Core\Http\{HttpResponse as Response};
 
 class HomeController
 {
-    public function sample(Request $request): ?Response
+    public function index(): ?Response
     {
-        # checking request method using the Request object
-        if ($request->is_get_request()) {
-
-            response()->out('hello'); // hello
-            response()->json(['result' => 'success']); // {'result': 'success}
-            response(['result' => 'success'])->toJson(); // array to json
-            response()->render('template.html'); // render template
-            response()->setStatusCode(200); // status code
-        }
-
-        return null;
+        return response('hello world')->send();
     }
 }
--- routes/main.route.php
+++ routes/main.route.php
@@ -8,30 +8,4 @@ use FastVolt\Router\Route;
 |--------------------------------------------------------------------------
 */
 
-Route::get('/', 'HomeController@index', name: 'home');
-Route::get('/test', function() {
-   $_SESSION['hxx'] = 'hello';
-   echo $_SESSION['hxx'];
-   unset($_SESSION['hxx']);
-});
-
-# Login Auth
-Route::mixed(['GET', 'POST'], '/login', 'LoginController@index', ['home.auth', 'verify.csrf'], name: 'login');
-
-# Sign Up Auth
-Route::mixed(['GET', 'POST'], '/register', 'RegisterController@index', 'home.auth', name: 'register');
-Route::post('/register/validate-mail', 'RegisterController@validate_mail', 'home.auth', name: 'register_validate_mail');
-
-# dashboard
-Route::group('/user', 'dash.auth')
-    -> get('/dashboard', 'DashboardController@index', name: 'dashboard')
-    -> get('/dashboard/stats', 'DashboardController@stats', name: 'dashboard_stats')
-    -> mixed(['GET', 'POST'], '/myfiles', 'MyFilesController@index', name: 'dash_myfiles')
-    -> get('/myfiles/fopt', 'MyFilesController@loadOptions', name: 'dash_myfiles_load_opt')
-    -> post('/myfiles/opt/rename', 'MyFilesController@renameFile', name: 'dash_update_file_name')
-    -> get('/myfiles/opt/preview/{id}', 'MyFilesController@previewFile', name: 'dash_myfiles_preview_file')
-    -> mixed(['GET', 'POST'], '/myfiles/opt/folder', 'FolderController@init', 'verify.csrf', name: 'dash_create_folder')
-    -> get('/myfiles/opt/folder/listall', 'FolderController@listAllFolders', name: 'dash_list_all_folders')
-    -> mixed(['GET', 'POST'], '/folder/{id:string}', 'FolderController@viewFolder', name: 'dash_folder')
-    -> post('/folder/action/delete', 'FolderController@deleteFolder', 'verify.csrf', name: 'dash_folder_delete')
-    -> mixed(['GET', 'POST'], '/upload', 'FileUploadController@uploadFilesInterface', name: 'dash_upload_files');
+Route::get('/', fn() => 'hello world!', name: 'home');
