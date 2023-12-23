230533ba377a0f733b9945cbb452a7a83777fd84
Oladoyinbo Vincent <oladoyinboadverts@gmail.com>
2023-12-23 12:40:37 +0100

    :moon: wip

--- app/Http/Controller/HomeController.php
+++ app/Http/Controller/HomeController.php
@@ -8,18 +8,8 @@ use Fastvolt\Core\Http\{HttpRequest as Request, HttpResponse as Response};
 
 class HomeController
 {
-    public function sample(Request $request): ?Response
+    public function index(Request $request): ?Response
     {
-        # checking request method using the Request object
-        if ($request->is_get_request()) {
-
-            response()->out('hello'); // hello
-            response()->json(['result' => 'success']); // {'result': 'success}
-            response(['result' => 'success'])->toJson(); // array to json
-            response()->render('template.html'); // render template
-            response()->setStatusCode(200); // status code
-        }
-
-        return null;
+        return response('hello world')->out();
     }
 }
